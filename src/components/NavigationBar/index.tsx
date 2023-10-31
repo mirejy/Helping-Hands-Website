6fe59f2e31db1bb0eeac6a2da8e6af06c6cb2cd5
varejy <lionvasia@mail.ru>
2023-10-31 14:41:21 +0100

    add hosting

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -10,7 +10,7 @@ export default function NavigationBar({ open }: Props) {
 		<NavContainer>
 			<UlNavigation open={open}>
 				<li><AnchorLink targetId="AboutUs"><Links>O nas</Links></AnchorLink></li>
-				<li><Links href="./Statut.pdf" download>Statut</Links></li>
+				<li><Links href="https://drive.google.com/file/d/1qoyyUGQug0ktf21NSSBS_i4UUOznFY5M/view?usp=sharing" target="_blank">Statut</Links></li>
 				<li><AnchorLink targetId="Contacts"><Links>Kontakty</Links></AnchorLink></li>
 			</UlNavigation>
 		</NavContainer>
